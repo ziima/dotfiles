1ee811899f941941ee72c86c6728a9d55c64e943
Vlastimil Zíma <vlastimil.zima@nic.cz>
2024-03-06 13:56:11 +0100

    Fix fish greeting

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,5 +1,5 @@
 # Disable greeting
-set -U fish_greetings
+set -U fish_greeting
 
 # Set prompt
 set -g tide_right_prompt_items status cmd_duration context jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig time
