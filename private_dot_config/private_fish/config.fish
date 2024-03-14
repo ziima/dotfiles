3d7d9473f18ad450808f624f40cd37a9548dcdf5
Vlastimil Zíma <vlastimil.zima@nic.cz>
2024-03-14 09:04:14 +0100

    Update $PATH

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,10 @@
 # Disable greeting
 set -U fish_greeting
 
+# Update $PATH
+fish_add_path -m $HOME/.local/bin
+fish_add_path -m $HOME/bin
+
 # Set prompt
 set -g tide_right_prompt_items status cmd_duration context jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig time
 set -g tide_prompt_add_newline_before true
