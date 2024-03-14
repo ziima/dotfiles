db64821511044b0255d6abe2be62a2dc6ba8ba4f
Vlastimil Zíma <vlastimil.zima@nic.cz>
2024-03-14 09:11:03 +0100

    Fix $PATH update

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,8 +2,8 @@
 set -U fish_greeting
 
 # Update $PATH
-fish_add_path -m $HOME/.local/bin
-fish_add_path -m $HOME/bin
+fish_add_path -P $HOME/.local/bin
+fish_add_path -P $HOME/bin
 
 # Set prompt
 set -g tide_right_prompt_items status cmd_duration context jobs direnv node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig time
